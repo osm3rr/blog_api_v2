183ec7303a9c1da3b3b46009c4277031ef2942f4
osm3rr <osm3rr@gmail.com>
2024-02-12 11:22:57 -0400

    blog api setup

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,19 +37,37 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
+    # 3rd party
+    'rest_framework',
+    'corsheaders',
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    "https://localhost:3000",
+    "https://localhost:8000",
+)
+
+CSRF_TRUSTED_ORIGINS = ["https://localhost:3000"]
 
 ROOT_URLCONF = 'django_project.urls'
 
